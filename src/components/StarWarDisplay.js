6a5683f83a9593518b1cf2c08ce80070d31fd9d2
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2022-12-18 16:25:50 -0800

    css update

--- src/components/StarWarDisplay.js
+++ src/components/StarWarDisplay.js
@@ -1,8 +1,9 @@
-const StarWarDisplay = ({ name }) => {
+const StarWarDisplay = ({ name,title}) => {
   // console.log(starWarsData)
   return (
     <div className="list">
       <h3>{name}</h3>
+      {/* <h4>{title}</h4> */}
     </div>
   );
 };
